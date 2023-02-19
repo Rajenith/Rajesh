febc20332702d0c2e94b249429e1b2fa589b3cbe
Rajesh~ <kiruthiraj2000@.com>
2023-02-19 16:11:19 +0530

    version 2

--- src/components/datas/single.js
+++ src/components/datas/single.js
@@ -1,11 +1,15 @@
-import React  from "react";
+import React, { useEffect }  from "react";
 import Datas from "./data";
 function Single() {
+    useEffect(()=>(
+        console.log("very bad condition")
+    ))
 
     return (
         <div>
             <div>i</div>
             <div>love you</div>
+            
         </div>
     )
 }
